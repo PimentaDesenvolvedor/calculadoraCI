21a88f6edeedeaa4479d7148618c3d093f6643ed
Ronan Souza Pimenta <ronanrj@gmail.com>
2023-11-28 16:01:05 -0300

    feat: :sparkles: Adiciona funcao soma

---
+++ Calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Type '<number1> <number2>'.");
+            // var input = Console.ReadLine();
+            // var inputArgs = input?.Split(' ');
+            // var number1 = Convert.ToInt32(inputArgs[0]);
+            // var number2 = Convert.ToInt32(inputArgs[1]);
+
+            calculaSoma(10, 10);
+        }
+
+        private static void calculaSoma(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
